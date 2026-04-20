5c03664cd191116a231ec29102416fee3cbf94c8
Adrián Alcalde Alzaga <aaa1013@alu.ubu.es>
2026-04-20 20:39:00 +0200

    chore: initialize Spring Boot project base

---
+++ src/main/java/es/universidad/adrian/distribuidos/PracticaSistemasDistribuidosApplication.java
@@ -0,0 +1,13 @@
+package es.universidad.adrian.distribuidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaSistemasDistribuidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaSistemasDistribuidosApplication.class, args);
+	}
+
+}
